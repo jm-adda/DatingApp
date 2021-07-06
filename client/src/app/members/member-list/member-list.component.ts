2ca2c7335959a11972cbdd3fde5da650fd7681da
Joan Marc Ortiz Radio <jayeme@MacBook-Pro-de-Joan.local>
2021-07-06 17:24:02 +0200

    End Section 9

--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -1,15 +1,25 @@
+import { ViewEncapsulation } from '@angular/compiler/src/core';
 import { Component, OnInit } from '@angular/core';
+import { Member } from 'src/app/_models/member';
+import { MembersService } from 'src/app/_services/members.service';
 
 @Component({
   selector: 'app-member-list',
   templateUrl: './member-list.component.html',
-  styleUrls: ['./member-list.component.css']
+  styleUrls: ['./member-list.component.css'],
 })
-export class MemberListComponent implements OnInit {
 
-  constructor() { }
+export class MemberListComponent implements OnInit {
+  members: Member[];
+
+  constructor(private memberService: MembersService) { }
 
   ngOnInit(): void {
+    this.loadMembers();
+  }
+
+  loadMembers() {
+    this.memberService.getMembers().subscribe(members => this.members = members);
   }
 
 }
